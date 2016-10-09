29d43fd7baa9e0894533011eab548a5c99a166af
wangqj <3001162649@qq.com>
2016-10-09 10:58:48 +0800

    更新cc

--- src/com/my/test/CC.java
+++ src/com/my/test/CC.java
@@ -3,12 +3,15 @@
  */
 package com.my.test;
 
-/** 
+/**
  * @Description: 这里用一句话描述这个类的作用
  * @see: CC 此处填写需要参考的类
- * @version 2016年10月9日 上午10:38:00 
+ * @version 2016年10月9日 上午10:38:00
  * @author wangqj
  */
 public class CC {
 
+	public void aa() {
+		System.out.println("lllll");
+	}
 }
